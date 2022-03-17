bda36c46918db7dd9c6baa9a7b1f906c867b91a5
Poyopote <94369664+Poyopote@users.noreply.github.com>
2022-03-17 20:47:05 +0100

    Create connexion.php

---
+++ Page/Utilisateur/connexion.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+    if(isset($_SESSION["message_erreur"])) 
+    {
+        $message_erreur = $_SESSION["message_erreur"];
+    }
+    else
+    {
+        $message_erreur = "";
+    }
+
+
+//     $password = "mdp";
+// echo "Password is:", $password;
+// echo "<br><br>";
+// echo "Hashed password using CRYPT_BLOWFISH: ",
+//     password_hash($password, PASSWORD_BCRYPT);
+// echo "<br><br>";
+ 
+// echo "Hashed password using Argon2i: ",
+//     password_hash($password, PASSWORD_ARGON2I);
+// echo "<br><br>";
+ 
+// echo "Hashed password using bcrypt: ",
+// $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+//     echo "<br><br>";
+
+//     if (password_verify($password, $hashed_password)) {
+//         echo 'Password is valid!';
+//     } else {
+//         echo 'Invalid password.';
+//     }
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="Language" content="fr" />
+    <meta name="copyright" content="© REEDIFICA.FR 2022" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#5b99c2">
+    <meta name="Author" content="Steven Ladour" />
+    <link rel="icon" href="../../img/Logo_favicon.svg">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="../../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../../css/user.css">
+    
+    <title>Page Connexion "Ravi de te voir" - Ré.édifica</title>
+    <!-- SEO  -->
+    <!-- <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/> -->
+    <meta property="og:region" content="fr_FR"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:site_name" content="Ré.édifica"/>
+    <meta name="description" content="Ré.édifica, le forum RP. "/>
+</head>
+<body>
+    <main id="connexion-main">
+        <nav>
+            <a href="../../index.php">Retour à l'accueil</a>
+            <a href="inscription.php">Je veux m'inscrire</a>
+        </nav>
+        <section>
+            <form id="formulaire" action="../../includes/connexion_inscription.php" method="POST">
+                <fieldset>
+                    <label for="login"><i class="bi bi-person-fill"></i> Pseudo :</label>
+                    <input type="text" id="login" name="login">
+                    <label for="mdp"><i class="bi bi-lock-fill"></i> Mot de passe :</label>
+                    <input type="password" id="mdp" name="mdp">
+                    <input name="formulaire" type="hidden" value="0">
+                    <p><?php printf($message_erreur); ?></p>
+                    <!-- <hr> -->
+                    <input type="submit" value="Valider">
+                </fieldset>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
