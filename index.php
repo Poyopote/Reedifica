28d30973a91e95a8b0f83701738734702dd1fb70
Valto X <visualy.no.ti@gmail.com>
2022-03-21 16:29:30 +0100

    début twig

--- index.php
+++ index.php
@@ -8,30 +8,11 @@
       $lien_user = '<a href="Page/Utilisateur/Profil.php">Profil</a> | <a href="includes/deconnexion.php">Déconnexion</a>';
     }
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="Language" content="fr" />
-    <meta name="copyright" content="© REEDIFICA.FR 2022" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="theme-color" content="#5b99c2">
-    <meta name="Author" content="Steven Ladour" />
-    <link rel="icon" href="img/Logo_favicon.svg">
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-    
-    <title>Accueil - Ré.édifica</title>
-    <!-- SEO  -->
-    <!-- <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large"/> -->
-    <meta property="og:region" content="fr_FR"/>
-    <meta property="og:type" content="website"/>
-    <meta property="og:site_name" content="Ré.édifica"/>
-    <meta name="description" content="Ré.édifica, le forum RP. "/>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-</head>
+<?php
+  include "includes/header.php"
+?>
 <body>
   <header id="banniere">
     <h1 class="logo">
