170b630a7b217813490c6ae87102755612869fdb
Poyopote <94369664+Poyopote@users.noreply.github.com>
2022-03-24 16:09:20 +0100

    Create profil_fonctions.php

---
+++ includes/profil_fonctions.php
@@ -0,0 +1,58 @@
+<?php
+    $user_pseudo = $_SESSION["login"];
+    $error ="";
+    // Vérifie s'il y une session utilisateur
+    if(empty($user_pseudo))
+    {
+      //pas connecter ? donc redirection vers la page de connexion
+      header("Location: ../../Page/Utilisateur/connexion.php");
+    }
+    //sinon tout va bien on change le menu de connexion.
+    else $lien_user = '<a href="Profil.php">Profil</a> | <a href="../../includes/deconnexion.php">Déconnexion</a>';
+  
+    $tableau_utilisateur = info_utilisateur_profil($bdd,$user_pseudo);
+    
+    $filename = '../../Docs/'.$user_pseudo;
+    
+  
+    if (isset($_POST['send'])) {
+  
+  
+  
+      if (file_exists($filename)) {
+        // echo "Le fichier $filename existe.";
+        // $test = $_POST['MAX_FILE_SIZE'];
+        $extensionsValides = array('jpg', 'jpeg', 'gif', 'png');//limite les types de fichiers qu'on importe --> l'user ne pourra pas importer un virus
+        $extensionUpload = strtolower(substr(strrchr($_FILES['userfile']['name'], '.'), 1)); //extension du fichier chargé. substr = ignorer un caractère (ici le premier de la chaîne car on a mis le 1 comme limite). strrchr = prendre l'extension du fichier (avec le point) puisqu'on prend à partir du point. '.' = caractère que la chaîne ne va pas prendre en compte. 1 = limite de la chaîne
+        // if($_FILES['userfile']['size'] <= $_POST['MAX_FILE_SIZE']) {
+          if($_FILES['userfile']['size'] <= 2000000 ) {
+          if(in_array($extensionUpload, $extensionsValides)) {//vérifie que l'extension du fichier chargé correspond aux extensions acceptées (d'abord la variable sur laquelle on applique la fonction in_array et ensuite les variables qu'on veut tester sur la chaîne
+            // $filename.".".$extensionUpload;//chemin où sera upload la photo
+            $new_image = $user_pseudo.".".$extensionUpload;
+            $resultat = move_uploaded_file($_FILES['userfile']['tmp_name'], $filename."/".$new_image);//déplacer le fichier de nom (paramètre1) jusqu'à (paramètre2). tmp_name = chemin temporaire du fichier
+            
+            if($resultat){//vérifier que le déplacement s'est bien effectué
+                image_de_profil($bdd,$user_pseudo,$new_image);
+                header('Location: Profil.php');
+            }
+            else{
+              $error = "<script> alert('Erreur durant l'importation de votre photo de profil');</script>";
+            }
+          }
+          else{
+            $error = "<script> alert('Votre photo de profil doit être au format jpg, jpeg, gif ou png');</script>";
+          }
+        }
+        else{
+          $error = "<script> alert('Poids limité à 2Mo');</script>";
+        }
+        
+  
+      }
+      else {
+        $error = "<script> alert('Recommence stp');</script>";
+        mkdir($filename);
+      }
+      // header('Location: Profil.php');
+    }
+?>
