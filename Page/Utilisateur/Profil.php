11976d1a327a09f90424f88cc67f1d8c244499b2
Poyopote <94369664+Poyopote@users.noreply.github.com>
2022-04-04 18:19:33 +0200

    css design footer

--- Page/Utilisateur/Profil.php
+++ Page/Utilisateur/Profil.php
@@ -41,17 +41,15 @@ else header("Location: ../../Page/Utilisateur/connexion.php");
 
 
     if (isset($_POST['send'])) {
-  
-  
-  
+
       if (file_exists($filename)) {
-        // echo "Le fichier $filename existe.";
-        // $test = $_POST['MAX_FILE_SIZE'];
         $extensionsValides = array('jpg', 'jpeg', 'gif', 'png');//limite les types de fichiers qu'on importe --> l'user ne pourra pas importer un virus
         $extensionUpload = strtolower(substr(strrchr($_FILES['userfile']['name'], '.'), 1)); //extension du fichier chargé. substr = ignorer un caractère (ici le premier de la chaîne car on a mis le 1 comme limite). strrchr = prendre l'extension du fichier (avec le point) puisqu'on prend à partir du point. '.' = caractère que la chaîne ne va pas prendre en compte. 1 = limite de la chaîne
+        
         // if($_FILES['userfile']['size'] <= $_POST['MAX_FILE_SIZE']) {
           if($_FILES['userfile']['size'] <= 3000000 ) {
           if(in_array($extensionUpload, $extensionsValides)) {//vérifie que l'extension du fichier chargé correspond aux extensions acceptées (d'abord la variable sur laquelle on applique la fonction in_array et ensuite les variables qu'on veut tester sur la chaîne
+            
             // $filename.".".$extensionUpload;//chemin où sera upload la photo
             $new_image = $user_pseudo.".".$extensionUpload;
             $resultat = move_uploaded_file($_FILES['userfile']['tmp_name'], $filename."/".$new_image);//déplacer le fichier de nom (paramètre1) jusqu'à (paramètre2). tmp_name = chemin temporaire du fichier
@@ -72,8 +70,7 @@ else header("Location: ../../Page/Utilisateur/connexion.php");
         else{
           $error = function_alert("Poids limité à 2Mo");
         }
-        
-  
+
       }
       else {
         $error = function_alert("Une erreur est survenu, veuillez recommencer");
