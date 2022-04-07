5f3fb31e204f0332dc0eb17c5be56a7657709f0b
Valto X <visualy.no.ti@gmail.com>
2022-04-07 17:46:28 +0200

    07/04

--- script/redaction.js
+++ script/redaction.js
@@ -2,9 +2,15 @@ tinymce.init({
   selector: 'textarea', //selecteur pour tiny
   language: 'fr_FR', // pour changer la langue du menu
   plugins: 'advlist link image lists  code save', //le plugine sont des modul qu'on rajoute
-  toolbar: "undo redo | image code | save",
+  // toolbar: "undo redo | save | image code | fontsizeselect| fontselect | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify | outdent indent",
+  toolbar: 'undo redo  | save | styleselect | forecolor | bold italic | alignleft aligncenter alignright | outdent indent | link image | code',
+  menubar: false,
   save_enablewhendirty: true,
+  fontsize_formats: '8pt 10pt 12pt 14pt 16pt 18pt 24pt 36pt 48pt',
   images_upload_url: '../../../includes/postAcceptor.php',
+  content_style:
+    "@import url('https://fonts.googleapis.com/css2?family=Outfit:wght@200&display=swap');",
+  font_formats: "Arial=arial,helvetica,sans-serif; Courier New=courier new,courier,monospace; Outfit=Outfit; Marck Script='Marck Script'",
   save_onsavecallback: function()
   {
     const rp = encodeURIComponent(tinyMCE.get('tiny').getContent());
@@ -33,8 +39,8 @@ tinymce.init({
     // xmlhttp.send(`text=${ text }`);
   },
   
-  menu:{
-    format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript | blockformats fontformats fontsizes align lineheight | forecolor backcolor | removeformat' },
-  }
+  // menu:{
+  //   format: { title: 'Format', items: 'bold italic underline strikethrough superscript subscript | blockformats fontformats fontsizes align lineheight | forecolor backcolor | removeformat' },
+  // }
   
 });
