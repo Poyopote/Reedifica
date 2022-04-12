9f50ff3c4a815ab90668d49b6870b4dea9e84183
Valto X <visualy.no.ti@gmail.com>
2022-04-12 11:11:28 +0200

    css exploration 2/2

--- includes/getmonde.php
+++ includes/getmonde.php
@@ -17,7 +17,8 @@ include("fonctions.php");
 
 
     echo "<h2>".$monde["name_world"]."</h2>";
-    echo "<img src='../../img/".$monde["media"]."' alt='illustration'>";
+    echo "<img class='font' src='../../img/".$monde["media"]."' alt='illustration'>";
+    echo "<img class='seul' src='../../img/".$monde["media"]."' alt='illustration'>";
     echo "<p>".$monde["bio_world"]."</p>";
     echo "<div class='clear'></div>";
     // print_r($choix);
