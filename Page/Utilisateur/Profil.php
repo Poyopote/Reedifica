e452ecb19be68dc55bd37c9b086259cfa28f4450
Poyopote <94369664+Poyopote@users.noreply.github.com>
2022-04-21 10:17:12 +0200

    Update Profil.php

--- Page/Utilisateur/Profil.php
+++ Page/Utilisateur/Profil.php
@@ -3,90 +3,30 @@
 
   include("../../includes/fonctions.php");
   require "../../includes/init_twig.php";
-//INCLUSION DE LA BDD
+
+//SESSION + INCLUSION DE LA BDD + Initialization
 
   include("../../includes/init_BDD.php");
+  session_start();
   $bdd = connexion_bdd();
 
-  session_start();
   $les_histoire = $error = $table_selectionnee = $lignes_values = $lignes_columns = "";
   $cle_primaire ="valeur par defaut";
-  $user_pseudo = user_connect();
-
   $mon_compte = false;
 
-
-// Vérifie si la personne est connecter
-if(isset($_SESSION["login"])){
-  //   Vérifie si c'est Le profil d'un autre
-  if (isset($_GET['utilisateur']) && $_GET['utilisateur'] != "" && info_utilisateur_profil($bdd,$_GET['utilisateur']) && $_GET['utilisateur'] != $user_pseudo) {
-    $tableau_utilisateur = info_utilisateur_profil($bdd,$_GET['utilisateur']);
-    $filename = '../../Docs/'.$_GET['utilisateur'];
-    $les_histoire = recherche_histoire_user($bdd,$tableau_utilisateur["id_user"]);
-  }
-  // ou son profil qu'il veut voir
-  else {
-    $tableau_utilisateur = info_utilisateur_profil($bdd,$user_pseudo);
-    $filename = '../../Docs/'.$user_pseudo;   
-    $mon_compte = true;
-    $les_histoire = recherche_histoire_user($bdd,$tableau_utilisateur["id_user"]);
-  }
-}
-
-// Sinon statut d'invité, vérifie si recherche_monde
-else if (isset($_GET['utilisateur']) && $_GET['utilisateur'] != "" && info_utilisateur_profil($bdd,$_GET['utilisateur'])){
-
-  $tableau_utilisateur = info_utilisateur_profil($bdd,$_GET['utilisateur']);
-  $filename = '../../Docs/'.$_GET['utilisateur'];
-  $les_histoire = recherche_histoire_user($bdd,$tableau_utilisateur["id_user"]);
-}
-// sinon propose de se connecter
-else header("Location: ../../Page/Utilisateur/connexion.php");
-
-if (isset($_POST['send']) && $_POST['send'] == "Modifier l'image") {
-
-  if (file_exists($filename)) {
-    $extensionsValides = array('jpg', 'jpeg', 'gif', 'png');//limite les types de fichiers qu'on importe --> l'user ne pourra pas importer un virus
-    $extensionUpload = strtolower(substr(strrchr($_FILES['userfile']['name'], '.'), 1)); //extension du fichier chargé. substr = ignorer un caractère (ici le premier de la chaîne car on a mis le 1 comme limite). strrchr = prendre l'extension du fichier (avec le point) puisqu'on prend à partir du point. '.' = caractère que la chaîne ne va pas prendre en compte. 1 = limite de la chaîne
-    
-    if($_FILES['userfile']['size'] <= 3000000 ) {
-      if(in_array($extensionUpload, $extensionsValides)) {//vérifie que l'extension du fichier chargé correspond aux extensions acceptées (d'abord la variable sur laquelle on applique la fonction in_array et ensuite les variables qu'on veut tester sur la chaîne
-        
-        // $filename.".".$extensionUpload;//chemin où sera upload la photo
-        $new_image = $user_pseudo.".".$extensionUpload;
-        $resultat = move_uploaded_file($_FILES['userfile']['tmp_name'], $filename."/".$new_image);//déplacer le fichier de nom (paramètre1) jusqu'à (paramètre2). tmp_name = chemin temporaire du fichier
-        
-        if($resultat){//vérifier que le déplacement s'est bien effectué
-            
-          image_de_profil($bdd,$user_pseudo,$new_image);
-            header('Location: Profil.php');
-        }
-        else  $error = function_alert("Erreur durant l'importation de votre photo de profil");
-          
-      }
-      else  $error = function_alert("Votre photo de profil doit être au format jpg, jpeg, gif ou png");
-    }
-    else  $error = function_alert("Poids limité à 2Mo");
-  }
-  else {
-    $error = function_alert("Une erreur est survenu, veuillez recommencer");
-    mkdir($filename);
-  }
-}
-if (isset($_POST['send']) && $_POST['send'] == "Modifier la description"){
-  image_de_bio($bdd,$user_pseudo,$_POST['bio']);
-  header('Location: Profil.php');
-}
-
-  $toto= nombre_user($bdd);
-  $toto[] = "salut";
-
+// BACK OFFICE
+  $BackOffice = new administration();
+  if( $mon_compte == true && isset($_POST['valider']) && ($_POST['valider'] == "story" || $_POST['valider'] == "rp"))
+    $BackOffice->clear($bdd);
+  
   $tables_req = $bdd->query("SHOW TABLES;");
   $lignes_tables = $tables_req->fetchAll();
+
   if(isset($_POST['valider'])){
     $table_selectionnee = $_POST['valider'];
     $values_req = $bdd->query("SELECT * FROM $table_selectionnee");
     $lignes_values = $values_req->fetchAll(PDO::FETCH_ASSOC);
+    
     $columns_req = $bdd->query("SHOW COLUMNS FROM $table_selectionnee");
     $lignes_columns = $columns_req->fetchAll();
 
@@ -94,30 +34,62 @@ if (isset($_POST['send']) && $_POST['send'] == "Modifier la description"){
     {
       if( $column['Key'] == "PRI" ) {
         $cle_primaire = $column['Field'];
-        var_dump($cle_primaire);
         break;
       }
       
     }
     foreach ($lignes_values as &$ligne) {
-
       $ligne['clef_primaire_extraite_de_la_reponse'] =  $ligne["$cle_primaire"] ;
     }
   }
 
-  $BackOffice = new administration();
-// if( $mon_compte == true && isset($_POST['valider']) && ($_POST['valider'] == "story" || $_POST['valider'] == "rp")){
-//   $BackOffice->clear($bdd);
- 
-// }
+// LES REQUETES
+  $user_pseudo = user_connect();
 
 
+  // Doit afficher les informations d'un utilisateur Présent dans la base de données.
+  // Connecté ?
+  if(isset($_SESSION["login"])){
+
+    // CAS 1: l'utilisateur est connecté et voir le profil d'un autre.
+    if (isset($_GET['utilisateur']) && $_GET['utilisateur'] != "" && info_utilisateur_profil($bdd,$_GET['utilisateur']) && $_GET['utilisateur'] != $user_pseudo) {
+      $tableau_utilisateur = info_utilisateur_profil($bdd,$_GET['utilisateur']);
+      $filename = '../../Docs/'.$_GET['utilisateur'];
+      $les_histoire = recherche_histoire_user($bdd,$tableau_utilisateur["id_user"]);
+    }
+    // CAS 2: l'utilisateur veut atteindre son profil.
+    else {
+      $tableau_utilisateur = info_utilisateur_profil($bdd,$user_pseudo);
+      $filename = '../../Docs/'.$user_pseudo;   
+      $mon_compte = true;
+      $les_histoire = recherche_histoire_user($bdd,$tableau_utilisateur["id_user"]);
+    }
+  }
+
+  // Invité ?
+  else if (isset($_GET['utilisateur']) && $_GET['utilisateur'] != "" && info_utilisateur_profil($bdd,$_GET['utilisateur'])){
+    $tableau_utilisateur = info_utilisateur_profil($bdd,$_GET['utilisateur']);
+    $filename = '../../Docs/'.$_GET['utilisateur'];
+    $les_histoire = recherche_histoire_user($bdd,$tableau_utilisateur["id_user"]);
+  }
+  // S'il url est bidon propose de se connecter.
+  else header("Location: ../../Page/Utilisateur/connexion.php");
+
+  include("../../includes/upload_image_profil.php");
+
+
+  
+  // Génération du Twig
   echo $twig->render('profil.html.twig', 
   array('lang' => $lang,
   'titre' => "Profil de ".$tableau_utilisateur["pseudo"]." - Ré.édifica",
   'css_style' => "../../css/style.css",
   'css_page' => "../../css/profil.css",
   'icon' => "../../img/Logo_favicon.svg",
+  "chemin_image_user" => "../../docs",
+  "reedifica" => "../../img/Logo_complet.svg",
+  
+  // menu de navigation
   'index' => "../../index.php",
   'contexte' => "../../Page/Histoire/contexte.php",
   'info' => "../../Page/Histoire/nouveauté/info.php",
@@ -130,10 +102,7 @@ if (isset($_POST['send']) && $_POST['send'] == "Modifier la description"){
   'deconnexion' => "../../includes/deconnexion.php",
   'connexion' => "connexion.php",
   'inscription' => "inscription.php",
-  "chemin_image_user" => "../../docs",
 
-  // lien footer
-  "reedifica" => "../../img/Logo_complet.svg",
 
   // donnée de la page
   'connecter' => !isset($_SESSION["login"]),
