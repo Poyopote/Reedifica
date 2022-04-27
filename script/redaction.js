84bbfcf248e77b72ec67618bc20d1161c967abf9
Poyopote <94369664+Poyopote@users.noreply.github.com>
2022-04-27 22:41:23 +0200

    Update redaction.js

--- script/redaction.js
+++ script/redaction.js
@@ -4,15 +4,17 @@ tinymce.init({
   height: 450,
   plugins: [
     'advlist autolink lists link image charmap print preview anchor save',
-    'searchreplace visualblocks code fullscreen',
+    'searchreplace visualblocks code fullscreen emoticons',
     'insertdatetime media table paste imagetools wordcount'
   ], //le plugine sont des modul qu'on rajoute
-  toolbar: 'undo redo | save | styleselect | forecolor | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image | code',
+  toolbar: 'undo redo | save | styleselect | forecolor backcolor | bold italic emoticons| alignleft aligncenter alignright | bullist numlist outdent indent | link image media | code',
   menubar: false,
   save_enablewhendirty: true,
   fontsize_formats: '8pt 10pt 12pt 14pt 16pt 18pt 24pt 36pt 48pt',
-  // images_upload_url: '../../includes/postAcceptor.php',
-  content_style:"@import url('https://fonts.googleapis.com/css2?family=Outfit:wght@200&display=swap');",
+  // images_upload_url: '../../includes/postAcceptor.php', fonctionne mais dangereux
+  
+  //ajout de police d'écriture
+  content_style:"@import url('https://fonts.googleapis.com/css2?family=Outfit:wght@200&display=swap'); @import url('https://fonts.googleapis.com/css2?family=Marck+Script&display=swap');",
   font_formats: "Arial=arial,helvetica,sans-serif; Courier New=courier new,courier,monospace; Outfit=Outfit; Marck Script='Marck Script'",
   save_onsavecallback: function()
   {
